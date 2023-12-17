49d3ccd964aca328d78a06c3b5523c37bd0ba339
Anbak98 <149641237+Anbak98@users.noreply.github.com>
2023-12-18 00:28:26 +0900

    Make tutorial directory

---
+++ Tutorial/main.js
@@ -0,0 +1,13 @@
+const config = {
+    type: Phaser.AUTO,
+    parent: 'app',
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 },
+        },
+    },
+    scene: [BasicWebGame]
+}
